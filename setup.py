ce07b62ef42aced758fc14308388cdb0f21a4312
Fake-Name <something@fake-url.com>
2018-06-09 22:58:37 -0700

    Can I tie the setup to the requirements.txt file?

--- setup.py
+++ setup.py
@@ -3,12 +3,16 @@ import setuptools
 from distutils.core import setup
 import sys
 import time
+
+install_requires = open("requirements.txt").readlines()
+install_requires = [tmp.strip() for tmp in install_requires]
+
 setup(
 	# Application name:
 	name="WebRequest",
 
 	# Version number (initial):
-	version="0.0.33",
+	version="0.0.35",
 
 	# Application author details:
 	author="Connor Wolf",
@@ -29,17 +33,6 @@ setup(
 	long_description              = open("README.md").read(),
 	long_description_content_type = "text/markdown",
 
-	dependency_links=[
-		'https://github.com/fake-name/ChromeController/tarball/master#egg=ChromeController-0.1.3',
-	],
-
 	# Dependent packages (distributions)
-	install_requires=[
-		'beautifulsoup4>=4.6.0',
-		'selenium>=3.8.1',
-		'PySocks>=1.6.8',
-		'cchardet>=2.1.1',
-		'lxml>=4.1.1',
-		"ChromeController>=0.2.0",
-	],
+	install_requires=install_requires,
 )
